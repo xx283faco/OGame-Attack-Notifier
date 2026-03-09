229cc914e9bc230052545f1790bd2e636a2fa2f5
xx283faco <xx283faco@gmail.com>
2026-03-09 15:18:43 +0100

    Add CORS headers to status API handler

--- api/status.js
+++ api/status.js
@@ -4,7 +4,14 @@
  */
 
 export default function handler(req, res) {
+  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
 
   res.status(200).json({
     status: 'ok',
